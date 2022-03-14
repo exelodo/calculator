9b60946e445ea6a93794fc44229e6d848021d813
unknown <SQLI@OUJD35H7T2.smb.sqli.com>
2022-03-14 18:13:02 +0100

    [fix]CALC-1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,8 @@
 public class Calculator {
 
-    public void add() {}
+    public void add() {
+        //if (b == 0) return 0
+    }
     public void subtraction(){}
     public void multiply() {}
     public void divide() {}
